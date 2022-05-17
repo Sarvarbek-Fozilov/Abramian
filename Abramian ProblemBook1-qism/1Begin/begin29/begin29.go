9898293d98df45a2401c40a04bb0a29284374e22
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-17 23:07:18 +0500

    gradusni radianga  otkazsh

---
+++ Abramian ProblemBook1-qism/1Begin/begin29/begin29.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+func main(){
+	var a float32
+	fmt.Println("A gradusning qiyamtini kiriting")
+	fmt.Scanf("f",&a)
+	degree:=(3.14*a)/180;
+
+	fmt.Println("ushbu gradusning radian qiymati:",degree)
+
+}
