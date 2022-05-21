d9cceadad5bd5dfacd13b0902b1e7a9bc118d710
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:25:13 +0500

    SM to Metr

---
+++ Abramian ProblemBook1-qism/2Integer/1integer/integer1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+func main(){
+	var sm int
+
+	fmt.Println("Necha sm ni Metrga aylantrmoqchisiz")
+	fmt.Scanf("%d",&sm)
+
+	fmt.Println("Ushbu sm ni Metrda quyidagicha",sm/100,"Metr")
+}
