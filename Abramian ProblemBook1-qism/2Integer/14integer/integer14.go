aedb260d0d00a40418ce8a3f27082a243620a347
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:44:29 +0500

    uch xonalik sonning ongdagisni chapga yozish

---
+++ Abramian ProblemBook1-qism/2Integer/14integer/integer14.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+func main(){
+	var son,yuzlik,onlik,birlik int
+	fmt.Println("uch xonalik son kiriting")
+	fmt.Scanf("%d",&son)
+	yuzlik=son/100
+	onlik=(son-yuzlik*100)/10
+	birlik=son-yuzlik*100 - onlik*10
+	fmt.Println("Ushbu uch xonalik sondi o'zgargan xolda  yozilishi:",yuzlik+onlik*10+birlik*100)
+
+}
