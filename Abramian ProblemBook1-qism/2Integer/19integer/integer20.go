66c65a86b7b936a4bf43dc6783f1b5b7aa75d91d
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:55:24 +0500

    Kun boshidan beri neccha minut o'tdi

---
+++ Abramian ProblemBook1-qism/2Integer/19integer/integer20.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n int
+	fmt.Println("Kun  boshidan boshlab necha  sekun o'tganini kiritng")
+	fmt.Scanf("%d",&n)
+	fmt.Println("Kun boshidan beri shuncha minut o'tdi",n/60)
+
+
+}
