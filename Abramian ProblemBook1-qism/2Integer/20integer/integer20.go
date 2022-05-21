73829ae4e14d5fb99a39bfd41784b4193f1f1485
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:59:35 +0500

    Kirtlgan sekun necha soatga tengligi

---
+++ Abramian ProblemBook1-qism/2Integer/20integer/integer20.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n int
+	fmt.Println("Kun  boshidan boshlab necha  sekun o'tganini kiritng")
+	fmt.Scanf("%d",&n)
+	fmt.Println("Kun boshidan beri shuncha soat o'tdi",n/360)
+
+
+}
