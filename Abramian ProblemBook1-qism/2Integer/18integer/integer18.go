807303c7a9fbaac904083819da5c137c555925d1
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:53:35 +0500

    Kiritilgan 4 xonalik sonning ming lik qiymati

---
+++ Abramian ProblemBook1-qism/2Integer/18integer/integer18.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main(){
+	var son,minglik int
+	fmt.Println("tort xonalik son kiriting")
+	fmt.Scanf("%d",&son)
+
+	minglik=son/1000
+
+	fmt.Println("Kiritilgan sonning minglik soni :",minglik)
+
+
+}
