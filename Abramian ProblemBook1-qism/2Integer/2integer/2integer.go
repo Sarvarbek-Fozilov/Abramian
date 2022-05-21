c36dbd8e529a1575a37197d8214298aaaa204d46
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:26:21 +0500

    Kg to tonna

---
+++ Abramian ProblemBook1-qism/2Integer/2integer/2integer.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+func main(){
+	var m int
+
+	fmt.Println("Necha kg ni to'nnaga aylantrmoqchisiz")
+	fmt.Scanf("%d",&m)
+
+	fmt.Println("Ushbu kg tonnada quyidagicha",m/1000,"tonna")
+}
