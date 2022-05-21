1bb59b2a8efddfeb634995f4449fc5f74027b4e7
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 16:20:04 +0500

    Joylashmay qolgan qismi

---
+++ Abramian ProblemBook1-qism/2Integer/29integer/integer29.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func main(){
+	var a,b,c int
+	fmt.Println("A ning qiymatini kiriring")
+	fmt.Scanf("%d",&a)
+	fmt.Println("B ning qiymatini kiriring")
+	fmt.Scanf("%d",&b)
+	fmt.Println("C ning qiymatini kiriring")
+	fmt.Scanf("%d",&c)
+
+
+	fmt.Println("Joylashmay qolgan qismi",(a/c)*(b/c))
+		
+	
+}
