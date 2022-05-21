e1c2f832d4bfe19297badf33e2eb798836e0721e
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-21 15:36:57 +0500

    # xonalik sonning yuzlik soni

---
+++ Abramian ProblemBook1-qism/2Integer/9integer/integer9.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+func main(){
+	var son  int
+	fmt.Println("uch xonalik son kiriting")
+	fmt.Scanf("%d",&son)
+	fmt.Println("kiritlgan sonning yuzlar xonasdagi son:",son/100)
+
+}
