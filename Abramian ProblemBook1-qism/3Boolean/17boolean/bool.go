3cf0cc3ec3be3846f9eb200501afc75ecb6376a3
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 11:16:30 +0500

    berilgan uch xonlaik son toq son

---
+++ Abramian ProblemBook1-qism/3Boolean/17boolean/bool.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "fmt"
+	func main(){
+		var a int 
+    fmt.Println("Musbat butun sondi kiritig")
+	fmt.Scanf("%d",&a)
+
+	if a>0 && a>=100 && a%2!=0{
+		fmt.Println("Berilgan son uch xonali toq")
+	} else {
+		fmt.Println("qayta urunib ko'ring")
+	}
+
+	}
