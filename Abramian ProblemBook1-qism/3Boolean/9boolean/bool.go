78eb20914b18137eb3ee3fb67e34bbab48567f1b
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 10:42:46 +0500

    A va b sonlarning hech bolmaganda bittasi toq son

---
+++ Abramian ProblemBook1-qism/3Boolean/9boolean/bool.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	  var  a,b int
+	  fmt.Println("A sonini kirting ")
+	  fmt.Scanf("%d",&a)
+	  fmt.Println("B sonini kirting")
+	  fmt.Scanf("%d",&b)
+
+
+	  fmt.Println(a%2!=0  || b%2!=0)
+}
