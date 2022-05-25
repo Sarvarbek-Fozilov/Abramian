97a955d17c3fd9c8868bc104fe80ec2965cb5ecb
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 10:40:17 +0500

    B soni a va c sonlar orasida yotadi shuni tekshrish

---
+++ Abramian ProblemBook1-qism/3Boolean/7boolean/bool.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	  var  a,b,c int
+	  fmt.Println("A sonini kirting ")
+	  fmt.Scanf("%d",&a)
+	  fmt.Println("B sonini kirting")
+	  fmt.Scanf("%d",&b)
+	  fmt.Println("C sonini kirting")
+	  fmt.Scanf("%d",&c)
+
+	  fmt.Println(a<b && b<c)
+}
