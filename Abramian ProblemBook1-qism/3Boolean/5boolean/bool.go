fbee582db3f0734f1a7e8eee0200c6aed0909bf9
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 10:37:47 +0500

    a>=0 || b< -2  rostlikga tekshirish

---
+++ Abramian ProblemBook1-qism/3Boolean/5boolean/bool.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	  var  a,b int
+	  fmt.Println("A sonini kirting ")
+	  fmt.Scanf("%d",&a)
+	  fmt.Println("B sonini kirting")
+	  fmt.Scanf("%d",&b)
+
+	  fmt.Println(a>=0 || b < -2)
+}
