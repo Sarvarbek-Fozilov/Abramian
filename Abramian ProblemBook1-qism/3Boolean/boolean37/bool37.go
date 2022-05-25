ff5e29184b9b6cd51a2575272b5b74b60d35ab26
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 12:13:49 +0500

    berilgan kordaatalari orqalik Shox bir yurshda bir maydondan ikkinchsga o'ta oladi tekshring:

---
+++ Abramian ProblemBook1-qism/3Boolean/boolean37/bool37.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func main(){
+	var x1,y1,x2,y2 int
+
+	fmt.Println("Shaxmat doskasining X1 kordintasi qiymatni kirit")
+	fmt.Scanf("%d",&x1)
+	fmt.Println("Shaxmat doskasining Y1 kordintasi qiymatni kirit")
+	fmt.Scanf("%d",&y1)
+
+	fmt.Println("Shaxmat doskasining X2 kordintasi qiymatni kirit")
+	fmt.Scanf("%d",&x2)
+	fmt.Println("Shaxmat doskasining Y2  kordintasi qiymatni kirit")
+	fmt.Scanf("%d",&y2)
+	
+
+	if (x1-x2)<=1 && (y1-y2)<=1 {
+		fmt.Printf("Tog'ri\n")
+	} else {
+		fmt.Printf("notog'ri\n")
+	}
+}
