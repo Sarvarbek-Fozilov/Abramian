767e94a15cba0580b13904214b5873ec2fa2e50e
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 11:27:23 +0500

    ushbu 3xonalik sonning barcha raqamlari teng

---
+++ Abramian ProblemBook1-qism/3Boolean/boolean20/bool20.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	var son,yuzlik,birlik,onlik int
+
+	fmt.Println("Uchxonalik sondi kiriting")
+	fmt.Scanf("%d",&son)
+
+	yuzlik= son/100
+	onlik= (son- yuzlik * 100)/10
+	birlik= (son- yuzlik * 100)- onlik*10
+
+	if yuzlik == onlik && onlik == birlik {
+		fmt.Println("Sonning barcha raqamlari teng")
+	} else{
+		fmt.Println("Boshqa son kiriting")
+	}
+
+}
