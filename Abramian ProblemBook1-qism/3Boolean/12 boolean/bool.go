213b2086dec7e6fb09f6ef536eeddd5a75d76751
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 10:46:50 +0500

    A,b,c sonlarning har biri musbat

---
+++ Abramian ProblemBook1-qism/3Boolean/12 boolean/bool.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	  var  a,b,c int
+	  fmt.Println("A sonini kirting ")
+	  fmt.Scanf("%d",&a)
+	  fmt.Println("B sonini kirting")
+	  fmt.Scanf("%d",&b)
+	  fmt.Println("C sonini kirting")
+	  fmt.Scanf("%d",&c)
+
+	   fmt.Println(a>0 && b>0 && c>0)
+	
+}
