7195bda1b19b0683c80d5148770d3fbd414e5c42
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 10:34:06 +0500

    A soni toq son ekanligi

---
+++ Abramian ProblemBook1-qism/3Boolean/2boolean/bool2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int
+	 fmt.Println("Juft yoki toq ekaligini bilmoqchi bolgan sondi kirit")
+	 fmt.Scanf("%d",&a)
+
+	 fmt.Println(a%2==0)
+
+
+}
