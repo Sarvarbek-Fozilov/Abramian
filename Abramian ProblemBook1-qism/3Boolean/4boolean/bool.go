573d97ea6f0ae4a9a2fe2660bfa071a3d3fd556d
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 10:36:20 +0500

    A>2 && b<=3 ni tekshirish

---
+++ Abramian ProblemBook1-qism/3Boolean/4boolean/bool.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+func main(){
+	var a,b int
+	fmt.Println("A sonni kirting")
+	fmt.Scanf("%d",&a)
+	fmt.Println("B sonini kirting")
+	fmt.Scanf("%d",&b)
+
+	fmt.Println(a>2 && b<=3)
+}
