be15fd1e98af1c6e04ba43898d09b0001180f11e
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-25 11:06:47 +0500

    kiritilgan son ikki xonalik va musbat eanligini tekshrish

---
+++ Abramian ProblemBook1-qism/3Boolean/16boolean/bool.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+func main(){
+	var a int
+	fmt.Println("Ikki xonalik musbat son kirting,kirtgan soniz musbat va ikki xonalik son bo'lsagina shart qanoatlantradi")
+	fmt.Scanf("%d",&a)
+
+
+	fmt.Println(a>0 && a>9 && a<100)
+
+}
