1ff32e57d0dc3da03ca9aff3e59fd91614ef3a8f
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-27 20:38:47 +0500

    ikta sondan kichgni tartb raqamani aniqlash

---
+++ Abramian ProblemBook1-qism/4if else/if7/if7.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x,y int
+
+	fmt.Println("Birinchi butun sondi kiriting")
+	fmt.Scanf("%d",&x)
+
+	fmt.Println("Ikkinnchi butun sondi kiriting")
+	fmt.Scanf("%d",&y)
+
+	if  x<y {
+		fmt.Println(1)
+	}else {
+		fmt.Println(2)
+
+	}
+
+
+}
