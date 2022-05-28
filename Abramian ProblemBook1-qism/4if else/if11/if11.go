22841980551a56e17449dc7b69c9f46fbaeb142a
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-28 23:49:54 +0500

    a b son teng bolsa iks sondan eng kattasi har birga ozlawtrsin,aksxolda nolga tenglashsin

---
+++ Abramian ProblemBook1-qism/4if else/if11/if11.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a,b int
+
+	fmt.Println("Birinchi  sondi kiriting")
+	fmt.Scanf("%d",&a)
+
+	fmt.Println("Ikkinnchi sondi kiriting")
+	fmt.Scanf("%d",&b)
+
+	if a!=b && a>b|| b>a{
+		a=(a>b) || a<b
+		b=a+b
+		fmt.Println(a,b)
+	
+	}else {
+		a=0
+		b=0
+		fmt.Println(a,b)
+
+	}
+
+
+}
