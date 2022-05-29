5143610882e412c96f5ca2c1824aff6d1ffb69ad
Sarvarbek-Fozilov <mohirdasturchi@gmail.com>
2022-05-29 22:34:02 +0500

    a>b>c yoki a<b<c bolsa xar birni *2 aks xolda *(-1)

---
+++ Abramian ProblemBook1-qism/4if else/if17/if17.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a,b,c float64
+
+	fmt.Println("Birinchi sondi kiriting")
+	fmt.Scanf("%f",&a)
+	fmt.Println("Ikkinchi sondi kiriting")
+	fmt.Scanf("%f",&b)
+	fmt.Println("Uchinchi sondi kiriting")
+	fmt.Scanf("%f",&c)
+
+	if (a<b && b<c) || c>b && b>a{
+		a=a*2
+		b=b*2
+		c=c*2
+		fmt.Println(a,b,c)
+	} else {
+		a=a*(-1)
+		b=b*(-1)
+		c=c*(-1)
+		fmt.Println(a,b,c)
+	}
+}
